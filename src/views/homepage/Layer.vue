52de6c2eb5ffd47b30f1cc5f6f88efb93a810e0d
杨小帅 <19962392@qq.com>
2021-09-03 21:11:46 +0800

    Changes

--- src/views/homepage/Layer.vue
+++ src/views/homepage/Layer.vue
@@ -95,4 +95,12 @@ export default {
     line-height: 200px;
     margin: 0;
   }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
 </style>
